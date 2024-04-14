3f81a2240b93f1d130c2b0f25e7ee6f890d058ce
Andrew Wong <andrew.wong.dgnt@gmail.com>
2024-04-13 23:07:07 -0700

    Added an entity. now its building

--- app/src/main/java/com/dgnt/quickScoreboardCreator/data/db/QSCDatabase.kt
+++ app/src/main/java/com/dgnt/quickScoreboardCreator/data/db/QSCDatabase.kt
@@ -2,7 +2,10 @@ package com.dgnt.quickScoreboardCreator.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.dgnt.quickScoreboardCreator.data.entity.ScoreboardEntity
+
 @Database(
+    entities = [ScoreboardEntity::class],
     version = 1
 )
 abstract class QSCDatabase: RoomDatabase() {
