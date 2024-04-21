1cb3833d2b9526bda7781b94174e1ed928791e3c
Andrew Wong <andrew.wong.dgnt@gmail.com>
2024-04-20 17:09:33 -0700

    Add sticky header add bottom padding for 100dp to the list so the FAB doesn't block the last item

--- app/src/main/java/com/dgnt/quickScoreboardCreator/ui/main/scoreboardlist/ScoreboardListContent.kt
+++ app/src/main/java/com/dgnt/quickScoreboardCreator/ui/main/scoreboardlist/ScoreboardListContent.kt
@@ -1,8 +1,15 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
 package com.dgnt.quickScoreboardCreator.ui.main.scoreboardlist
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
@@ -14,13 +21,17 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.dgnt.quickScoreboardCreator.common.util.UiEvent
 
@@ -62,27 +73,45 @@ fun ScoreboardListContent(
                     contentDescription = "Add"
                 )
             }
-        },
+        }
     ) { padding ->
-
         LazyColumn(
             modifier = Modifier
                 .padding(padding)
+                .padding(bottom=100.dp)
                 .fillMaxSize()
         ) {
-            items(scoreboardList.value) { scoreboard ->
-                ScoreboardItemContent(
-                    item = scoreboard,
-                    onEvent = viewModel::onEvent,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable {
-                            viewModel.onEvent(ScoreboardListEvent.OnLaunch(scoreboard))
-                        }
-                )
-            }
+            listOf(
+                Pair("Default", listOf()),
+                Pair("Custom", scoreboardList.value)
+            )
+                .forEach {
+                    val title = it.first
+                    stickyHeader {
+                        Text(
+                            text = title,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(MaterialTheme.colorScheme.primaryContainer)
+                                .padding(16.dp)
+                        )
+                    }
+                    val itemList = it.second
+                    items(itemList) { scoreboard ->
+                        ScoreboardItemContent(
+                            item = scoreboard,
+                            onEvent = viewModel::onEvent,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clickable {
+                                    viewModel.onEvent(ScoreboardListEvent.OnLaunch(scoreboard))
+                                }
+                        )
+                    }
+                }
         }
-
     }
 
 }
