d6b87443489ca54ab61e608033efbcb547646dd1
Andrew Wong <andrew.wong.dgnt@gmail.com>
2024-05-19 10:22:09 -0700

    update some tests to test out more cases for deuce/adv

--- app/src/test/java/com/dgnt/quickScoreboardCreator/domain/scoreboard/business/logic/QSCScoreboardManagerTest.kt
+++ app/src/test/java/com/dgnt/quickScoreboardCreator/domain/scoreboard/business/logic/QSCScoreboardManagerTest.kt
@@ -336,7 +336,18 @@ class QSCScoreboardManagerTest {
             ) to
                     IntervalData(
                         0, 0
-                    )
+                    ),
+            ScoreInfo(
+                ScoreRule.ScoreRuleTrigger.DeuceAdvantageRule(2),
+                mapOf(),
+                listOf(
+                    ScoreData(0, 0, listOf(2)),
+                    ScoreData(0, 0, listOf(2)),
+                )
+            ) to
+                    IntervalData(
+                        5, 5
+                    ),
         )
         run `increase player 1 by 1`@{
             sut.updateScore(0, 0)
@@ -473,7 +484,48 @@ class QSCScoreboardManagerTest {
             verify(exactly = 1) { sut.teamSizeUpdateListener?.invoke(2) }
             resetMocks()
         }
+        run `increase player 2 by 1`@{
+            sut.updateScore(1, 0)
+            verify(exactly = 1) {
+                sut.scoresUpdateListener?.invoke(
+                    DisplayedScoreInfo(
+                        displayedScores = listOf(
+                            DisplayedScore.CustomDisplayedScore("1"),
+                            DisplayedScore.CustomDisplayedScore("2"),
+                        ),
+                        overallDisplayedScore = DisplayedScore.Blank
+                    )
+                )
+            }
+            resetMocks()
+        }
 
+        run `increase player 2 by 1`@{
+            sut.updateScore(1, 0)
+            verify(exactly = 1) {
+                sut.scoresUpdateListener?.invoke(
+                    DisplayedScoreInfo(
+                        displayedScores = listOf(
+                            DisplayedScore.CustomDisplayedScore("0"),
+                            DisplayedScore.CustomDisplayedScore("0"),
+                        ),
+                        overallDisplayedScore = DisplayedScore.Blank
+                    )
+                )
+            }
+            verify(exactly = 1) { sut.timeUpdateListener?.invoke(5L) }
+            verify(exactly = 1) { sut.intervalIndexUpdateListener?.invoke(2) }
+            verify(exactly = 1) {
+                sut.incrementListUpdateListener?.invoke(
+                    listOf(
+                        listOf(2),
+                        listOf(2),
+                    )
+                )
+            }
+            verify(exactly = 1) { sut.teamSizeUpdateListener?.invoke(2) }
+            resetMocks()
+        }
 
     }
 
