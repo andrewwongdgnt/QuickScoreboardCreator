6a74fcee03809ca962084d1ca88fe988184ca18a
Andrew Wong <andrew.wong.dgnt@gmail.com>
2024-07-28 15:40:41 -0700

    fix unit tests

--- app/src/test/java/com/dgnt/quickScoreboardCreator/domain/history/business/logic/QSCHistoryCategorizerTest.kt
+++ app/src/test/java/com/dgnt/quickScoreboardCreator/domain/history/business/logic/QSCHistoryCategorizerTest.kt
@@ -27,15 +27,15 @@ class QSCHistoryCategorizerTest {
 
         sut(
             listOf(
-                HistoryEntity(id = 1, title = "Basketball", icon = ScoreboardIcon.BASKETBALL, lastLookedAt = DateTime(2024, 1, 15, 12, 10), HistoricalScoreboardData(mapOf())),
-                HistoryEntity(id = 2, title = "Spikeball", icon = ScoreboardIcon.BASKETBALL, lastLookedAt = DateTime(2024, 2, 14, 11, 0), HistoricalScoreboardData(mapOf())),
+                HistoryEntity(id = 1, title = "Basketball", icon = ScoreboardIcon.BASKETBALL, lastModified = DateTime(2024, 1, 15, 12, 10), createdAt = DateTime(2024, 1, 15, 12, 10), historicalScoreboard = HistoricalScoreboardData(mapOf()), temporary = false),
+                HistoryEntity(id = 2, title = "Spikeball", icon = ScoreboardIcon.BASKETBALL, lastModified = DateTime(2024, 2, 14, 11, 0), createdAt = DateTime(2024, 2, 14, 11, 0), historicalScoreboard = HistoricalScoreboardData(mapOf()), temporary = false),
             )
         ).let {
             Assert.assertEquals(2, it.size)
-            Assert.assertEquals(DateTime(2024, 2, 1, 0,0), it[0].dateTime)
+            Assert.assertEquals(DateTime(2024, 2, 1, 0, 0), it[0].dateTime)
             Assert.assertEquals(1, it[0].historyItemDataList.size)
             Assert.assertEquals(2, it[0].historyItemDataList[0].id)
-            Assert.assertEquals(DateTime(2024, 1, 1, 0,0), it[1].dateTime)
+            Assert.assertEquals(DateTime(2024, 1, 1, 0, 0), it[1].dateTime)
             Assert.assertEquals(1, it[1].historyItemDataList.size)
             Assert.assertEquals(1, it[1].historyItemDataList[0].id)
         }
@@ -48,18 +48,18 @@ class QSCHistoryCategorizerTest {
 
         sut(
             listOf(
-                HistoryEntity(id = 1, title = "Basketball", icon = ScoreboardIcon.BASKETBALL, lastLookedAt = DateTime(2024, 1, 4, 12, 10), HistoricalScoreboardData(mapOf())),
-                HistoryEntity(id = 2, title = "Spikeball", icon = ScoreboardIcon.BASKETBALL, lastLookedAt = DateTime(2024, 2, 16, 11, 0), HistoricalScoreboardData(mapOf())),
-                HistoryEntity(id = 3, title = "Volleyball", icon = ScoreboardIcon.VOLLEYBALL, lastLookedAt = DateTime(2024, 1, 19, 12, 10), HistoricalScoreboardData(mapOf())),
-                HistoryEntity(id = 4, title = "Tennis", icon = ScoreboardIcon.TENNIS, lastLookedAt = DateTime(2024, 2, 1, 5, 13), HistoricalScoreboardData(mapOf())),
+                HistoryEntity(id = 1, title = "Basketball", icon = ScoreboardIcon.BASKETBALL, lastModified = DateTime(2024, 1, 4, 12, 10), createdAt = DateTime(2024, 1, 4, 12, 10), historicalScoreboard = HistoricalScoreboardData(mapOf()), temporary = false),
+                HistoryEntity(id = 2, title = "Spikeball", icon = ScoreboardIcon.BASKETBALL, lastModified = DateTime(2024, 2, 16, 11, 0), createdAt = DateTime(2024, 2, 16, 11, 0), historicalScoreboard = HistoricalScoreboardData(mapOf()), temporary = false),
+                HistoryEntity(id = 3, title = "Volleyball", icon = ScoreboardIcon.VOLLEYBALL, lastModified = DateTime(2024, 1, 19, 12, 10), createdAt = DateTime(2024, 1, 19, 12, 10), historicalScoreboard = HistoricalScoreboardData(mapOf()), temporary = false),
+                HistoryEntity(id = 4, title = "Tennis", icon = ScoreboardIcon.TENNIS, lastModified = DateTime(2024, 2, 1, 5, 13),createdAt = DateTime(2024, 2, 1, 5, 13), historicalScoreboard = HistoricalScoreboardData(mapOf()), temporary = false),
             )
         ).let {
             Assert.assertEquals(2, it.size)
-            Assert.assertEquals(DateTime(2024, 2, 1, 0,0), it[0].dateTime)
+            Assert.assertEquals(DateTime(2024, 2, 1, 0, 0), it[0].dateTime)
             Assert.assertEquals(2, it[0].historyItemDataList.size)
             Assert.assertEquals(2, it[0].historyItemDataList[0].id)
             Assert.assertEquals(4, it[0].historyItemDataList[1].id)
-            Assert.assertEquals(DateTime(2024, 1, 1, 0,0), it[1].dateTime)
+            Assert.assertEquals(DateTime(2024, 1, 1, 0, 0), it[1].dateTime)
             Assert.assertEquals(2, it[1].historyItemDataList.size)
             Assert.assertEquals(3, it[1].historyItemDataList[0].id)
             Assert.assertEquals(1, it[1].historyItemDataList[1].id)
