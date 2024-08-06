9a6f506d754dc1c9f8112506a7971551968f4042
Andrew Wong <andrew.wong.dgnt@gmail.com>
2024-08-05 21:49:55 -0700

    complete adding unit tests to TimelineViewerViewModelTest

--- app/src/test/java/com/dgnt/quickScoreboardCreator/ui/scoreboard/timelineviewer/TimelineViewerViewModelTest.kt
+++ app/src/test/java/com/dgnt/quickScoreboardCreator/ui/scoreboard/timelineviewer/TimelineViewerViewModelTest.kt
@@ -4,9 +4,17 @@ import androidx.lifecycle.SavedStateHandle
 import com.dgnt.quickScoreboardCreator.data.history.data.HistoricalScoreboardData
 import com.dgnt.quickScoreboardCreator.data.history.entity.HistoryEntity
 import com.dgnt.quickScoreboardCreator.domain.common.mapper.Mapper
+import com.dgnt.quickScoreboardCreator.domain.history.model.HistoricalInterval
+import com.dgnt.quickScoreboardCreator.domain.history.model.HistoricalIntervalRange
+import com.dgnt.quickScoreboardCreator.domain.history.model.HistoricalScore
+import com.dgnt.quickScoreboardCreator.domain.history.model.HistoricalScoreGroup
 import com.dgnt.quickScoreboardCreator.domain.history.model.HistoricalScoreboard
+import com.dgnt.quickScoreboardCreator.domain.history.model.IntervalLabel
+import com.dgnt.quickScoreboardCreator.domain.history.model.TeamLabel
 import com.dgnt.quickScoreboardCreator.domain.history.usecase.GetHistoryUseCase
 import com.dgnt.quickScoreboardCreator.domain.scoreboard.model.ScoreboardIcon
+import com.dgnt.quickScoreboardCreator.domain.scoreboard.model.config.ScoreboardType
+import com.dgnt.quickScoreboardCreator.domain.team.model.TeamIcon
 import com.dgnt.quickScoreboardCreator.ui.common.Arguments
 import com.dgnt.quickScoreboardCreator.ui.common.uievent.UiEvent
 import com.dgnt.quickScoreboardCreator.ui.common.uievent.UiEventHandler
@@ -21,6 +29,7 @@ import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.joda.time.DateTime
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 
@@ -41,6 +50,76 @@ class TimelineViewerViewModelTest {
     private lateinit var uiEventHandler: UiEventHandler
 
     private lateinit var sut: TimelineViewerViewModel
+
+    private val mockHistoricalInterval1 = HistoricalInterval(
+        range = HistoricalIntervalRange.CountDown(72000),
+        intervalLabel = IntervalLabel.Custom("Quarter", 0),
+        historicalScoreGroupList = mapOf(
+            0 to HistoricalScoreGroup(
+                teamLabel = TeamLabel.Custom("DGNT", TeamIcon.AXE),
+                primaryScoreList = listOf(
+                    HistoricalScore(0, "0", 720000),
+                    HistoricalScore(1, "1", 660000),
+                    HistoricalScore(2, "2", 630000),
+                    HistoricalScore(3, "3", 480000),
+                    HistoricalScore(4, "4", 330000),
+                    HistoricalScore(7, "7", 300000),
+                ),
+                secondaryScoreList = listOf(
+                    HistoricalScore(1, "1", 720000)
+                )
+            ),
+            1 to HistoricalScoreGroup(
+                teamLabel = TeamLabel.None,
+                primaryScoreList = listOf(
+                    HistoricalScore(0, "0", 0),
+                    HistoricalScore(1, "1", 2000),
+                    HistoricalScore(2, "2", 4400),
+                    HistoricalScore(3, "3", 5655),
+                    HistoricalScore(4, "4", 9800),
+                ),
+                secondaryScoreList = listOf()
+            )
+        )
+    )
+
+    private val mockHistoricalInterval2 = HistoricalInterval(
+        range = HistoricalIntervalRange.Infinite,
+        intervalLabel = IntervalLabel.ScoreboardType(ScoreboardType.SPIKEBALL, 1),
+        historicalScoreGroupList = mapOf()
+    )
+
+    private val mockHistoricalInterval3 = HistoricalInterval(
+        range = HistoricalIntervalRange.CountDown(72000),
+        intervalLabel = IntervalLabel.Custom("Quarter", 0),
+        historicalScoreGroupList = mapOf(
+            0 to HistoricalScoreGroup(
+                teamLabel = TeamLabel.Custom("DGNT", TeamIcon.AXE),
+                primaryScoreList = listOf(
+                    HistoricalScore(0, "10", 720000),
+                    HistoricalScore(1, "11", 660000),
+                    HistoricalScore(2, "12", 640000),
+                    HistoricalScore(3, "13", 480000),
+                    HistoricalScore(4, "14", 330000),
+                    HistoricalScore(7, "17", 320000),
+                ),
+                secondaryScoreList = listOf(
+                    HistoricalScore(1, "11", 2)
+                )
+            ),
+            1 to HistoricalScoreGroup(
+                teamLabel = TeamLabel.None,
+                primaryScoreList = listOf(
+                    HistoricalScore(0, "0", 0),
+                    HistoricalScore(1, "1", 2000),
+                    HistoricalScore(2, "2", 4400),
+                    HistoricalScore(3, "3", 5655),
+                    HistoricalScore(4, "4", 9800),
+                ),
+                secondaryScoreList = listOf()
+            )
+        )
+    )
 
     private fun initSut() {
         sut = TimelineViewerViewModel(
@@ -69,8 +148,20 @@ class TimelineViewerViewModelTest {
                 temporary = false
             )
         }
-        every { historyScoreboardDomainMapper.map(any())} returns HistoricalScoreboard(mapOf()) //TODO create a proper mock object for the return
+        every { historyScoreboardDomainMapper.map(any())} returns HistoricalScoreboard(
+            mapOf(
+                0 to mockHistoricalInterval1,
+                1 to mockHistoricalInterval2,
+                2 to mockHistoricalInterval3,
+            )
+        ) 
 
+    }
+
+    @Test
+    fun testInit() = runTest {
+        initSut()
+        Assert.assertEquals(mockHistoricalInterval1, sut.historicalInterval.value)
     }
 
     @Test
@@ -80,5 +171,21 @@ class TimelineViewerViewModelTest {
         verify(exactly = 1) {
             sut.sendUiEvent(UiEvent.Done)
         }
+    }
+
+    @Test
+    fun testOnNewInterval() = runTest {
+        initSut()
+        sut.onNewInterval(true)
+        Assert.assertEquals(mockHistoricalInterval2, sut.historicalInterval.value)
+        sut.onNewInterval(true)
+        Assert.assertEquals(mockHistoricalInterval3, sut.historicalInterval.value)
+        sut.onNewInterval(false)
+        Assert.assertEquals(mockHistoricalInterval2, sut.historicalInterval.value)
+        sut.onNewInterval(true)
+        sut.onNewInterval(true)
+        Assert.assertEquals(mockHistoricalInterval1, sut.historicalInterval.value)
+        sut.onNewInterval(false)
+        Assert.assertEquals(mockHistoricalInterval3, sut.historicalInterval.value)
     }
 }
