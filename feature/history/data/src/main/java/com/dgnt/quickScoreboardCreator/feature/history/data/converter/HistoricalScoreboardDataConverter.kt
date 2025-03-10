6f34f7ee22c4b5b0b380527c76e9056de3ee00ab
Andrew Wong <andrew.wong.dgnt@gmail.com>
2025-03-09 21:05:50 -0700

    remove unecessary code

--- feature/history/data/src/main/java/com/dgnt/quickScoreboardCreator/feature/history/data/converter/HistoricalScoreboardDataConverter.kt
+++ feature/history/data/src/main/java/com/dgnt/quickScoreboardCreator/feature/history/data/converter/HistoricalScoreboardDataConverter.kt
@@ -4,18 +4,12 @@ import androidx.room.ProvidedTypeConverter
 import androidx.room.TypeConverter
 import com.dgnt.quickScoreboardCreator.core.data.serializer.Serializer
 import com.dgnt.quickScoreboardCreator.feature.history.data.entity.HistoricalScoreboardData
-import com.dgnt.quickScoreboardCreator.feature.sport.domain.model.interval.IntervalData
-import com.dgnt.quickScoreboardCreator.feature.sport.domain.model.score.ScoreInfo
-import com.dgnt.quickScoreboardCreator.feature.sport.domain.model.score.ScoreRule
 import com.google.gson.reflect.TypeToken
 
 @ProvidedTypeConverter
 class HistoricalScoreboardDataConverter(private val serializer: Serializer) {
     @TypeConverter
     fun fromJson(json: String): HistoricalScoreboardData {
-       val s =  ScoreInfo(ScoreRule.None, mapOf(), "", listOf())
-        val i = IntervalData(0,0)
-
         return serializer.deserialize(json, object : TypeToken<HistoricalScoreboardData>() {}.type)
     }
 
